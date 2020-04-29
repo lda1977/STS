2d047f443b6629b42712687900b40425f2374760
Luciano Amarante <lucianoa2028@gmail.com>
2020-04-28 23:25:49 -0300

    Beta Com boot Dev Tool Data JPA Flywar Migration MySql Driver

--- src/main/java/com/alaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/alaworks/osworks/api/controller/ClienteController.java
@@ -3,31 +3,27 @@ package com.alaworks.osworks.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alaworks.osworks.domain.model.Cliente;
+import com.alaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
+
+	@Autowired
+	private ClienteRepository ClienteRepository;
 	
 	@GetMapping("/clientes")
-	public List<Cliente> listar() {
-
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("joca que Furi");
-		cliente1.setTelefone("41-55555-9999");
-		cliente1.setEmail("juca@gmail.com");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria Rosa");
-		cliente2.setTelefone("44-22222-8888");
-		cliente2.setEmail("maria@gmail.com");
-		
-		/* Retorna uma lista com cliente1 e cliente2 */
-		return Arrays.asList(cliente1, cliente2);
+	public List<Cliente> listar() {	
+		return ClienteRepository.findAll();
+		//return ClienteRepository.findByNomeContaining("Ju");
 	}
 
 }
--- src/main/java/com/alaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/alaworks/osworks/domain/model/Cliente.java
@@ -1,14 +1,26 @@
 package com.alaworks.osworks.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 	
 	/* Atributos privados */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
+	/* fazendo um mapeamento para o banco pois mudou de telefone para fone no banco de dados */
+	@Column(name ="fone")
 	private String telefone;
 	
-	/* Metodos gerado */
+	/* Metodos gerado Get e Set */
 	public Long getId() {
 		return id;
 	}
@@ -33,8 +45,30 @@ public class Cliente {
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
-	
-	
+	/* Criando o hashCode para poder comparar os OBJ */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	/* Criando o equals para poder comparar os OBJ */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
